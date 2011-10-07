b9e2c9351a33a8f28ee157aaf3e2fe224a1b1dd1
Clayton Parker <clayton@sixfeetup.com>
2011-10-07 10:26:47 -0400

    add initial skeleton

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+version = '1.0'
+
+setup(
+    name='sixieskel.buildout',
+    version=version,
+    description="",
+    long_description=open('README.rst').read(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords='web zope command-line skeleton project',
+    author='Six Feet Up, Inc.',
+    author_email='info@sixfeetup.com',
+    url='http://www.sixfeetup.com',
+    namespace_packages=['sixieskel'],
+    packages=find_packages(exclude=['ez_setup']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        #"templer.buildout",
+    ],
+    entry_points="""
+    [paste.paster_create_template]
+    sfu_buildout = sixieskel.buildout.template:SixieBuildout
+    """,
+    )
