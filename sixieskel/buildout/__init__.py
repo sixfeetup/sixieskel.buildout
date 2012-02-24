0ed4837fdeb16f97e54c26fd68fd54b60cff288b
Clayton Parker <clayton@sixfeetup.com>
2012-02-23 20:02:15 -0500

    patch should_skip_file to handle hidden files

--- sixieskel/buildout/__init__.py
+++ sixieskel/buildout/__init__.py
@@ -1,1 +1,19 @@
-# look at my package
+import os
+from paste.script import copydir
+
+
+def should_skip_file(name):
+    """Overriding this just so we can include hidden files
+    """
+    if name.endswith('~') or name.endswith('.bak'):
+        return 'Skipping backup file %(filename)s'
+    if name.endswith('.pyc') or name.endswith('.pyo'):
+        return 'Skipping %s file %%(filename)s' % os.path.splitext(name)[1]
+    if name.endswith('$py.class'):
+        return 'Skipping $py.class file %(filename)s'
+    if name in ('CVS', '_darcs', '.svn', '.git', '.hg'):
+        return 'Skipping version control directory %(filename)s'
+    return None
+
+
+copydir.should_skip_file = should_skip_file
